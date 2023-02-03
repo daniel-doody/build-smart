cad429889eeb4920e8c0741a064975aac983ee17
daniel-doody <daniel.doody@ymail.com>
2023-02-03 13:04:39 -0500

    hero section change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Demo from './components/home/Demo';
 import Hero from './components/home/Hero';
-import Navbar from './components/home/Navbar';
 import ProfileCard from './components/home/ProfileCard';
 
 function App() {
   return (
     <div className='bg-gray-800'>
-      <Navbar />
       <Hero />
       <Demo />
       <ProfileCard />
--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,26 +1,103 @@
-import React from 'react';
+import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-  return (
-    <div className='bg-gray-900 h-20 shadow-sm flex px-20 justify-between'>
-      {/* LOGO */}
-      <div className='flex items-center '>
-        <h1 className='text-amber-500 text-2xl font-semibold font-sans'>
-          Build Smart
-        </h1>
-      </div>
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-      {/* BUTTONS */}
-      <div className='flex my-auto'>
-        <button className='text-white py-2 hover:scale-105 pr-6'>
-          Sign In
-        </button>
-        <button className='text-white bg-amber-500 hover:bg-amber-400 rounded-md shadow-sm py-3 px-8 hover:scale-105 '>
-          Create Project
-        </button>
-      </div>
+  const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Documentation', href: '#' },
+  ];
+
+  return (
+    <div className='px-6 pt-6 lg:px-8'>
+      <nav className='flex items-center justify-between' aria-label='Global'>
+        <div className='flex lg:flex-1'>
+          <a href='#' className='-m-1.5 p-1.5'>
+            <span className='sr-only'>Build Smart</span>
+            <h2 className='text-2xl font-bold drop-shadow-md'>Build Smart</h2>
+          </a>
+        </div>
+        <div className='flex lg:hidden'>
+          <button
+            type='button'
+            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className='sr-only'>Open main menu</span>
+            <Bars3Icon className='h-6 w-6' aria-hidden='true' />
+          </button>
+        </div>
+
+        {/* iterate through nav array to show links */}
+        <div className='hidden lg:flex lg:gap-x-12'>
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className='text-md font-semibold leading-6 text-gray-900'
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
+          <a href='#' className='text-sm font-semibold leading-6 text-gray-900'>
+            Log in <span aria-hidden='true'>&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog as='div' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog.Panel
+          focus='true'
+          className='fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden'
+        >
+          <div className='flex items-center justify-between'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img
+                className='h-8'
+                src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                alt=''
+              />
+            </a>
+            <button
+              type='button'
+              className='-m-2.5 rounded-md p-2.5 text-gray-700'
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className='sr-only'>Close menu</span>
+              <XMarkIcon className='h-6 w-6' aria-hidden='true' />
+            </button>
+          </div>
+          <div className='mt-6 flow-root'>
+            <div className='-my-6 divide-y divide-gray-500/10'>
+              <div className='space-y-2 py-6'>
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className='-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10'
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className='py-6'>
+                <a
+                  href='#'
+                  className='-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10'
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </div>
   );
 };
