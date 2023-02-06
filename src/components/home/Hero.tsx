8ec534815fda4256f310329a081fef9e7c14cf25
daniel-doody <daniel.doody@ymail.com>
2023-02-05 23:53:59 -0500

    hero content edit

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,18 +1,7 @@
 import { useState } from 'react';
-import { Dialog } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Navbar from './Navbar';
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-];
-
 const Hero = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <div className='isolate bg-white'>
       <div className='absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]'>
@@ -48,7 +37,7 @@ const Hero = () => {
           <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
             <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
               <div className='relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
-                Announcing our next round of funding.{' '}
+                BuildSmart is an open source 3D GIS software.{' '}
                 <a href='#' className='font-semibold text-indigo-600'>
                   <span className='absolute inset-0' aria-hidden='true' />
                   Read more <span aria-hidden='true'>&rarr;</span>
